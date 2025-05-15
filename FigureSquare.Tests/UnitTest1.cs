349c74dd5287f20458443005697126485d645ac0
EvgeniiBelov <evgeniibelov@gmail.com>
2025-05-15 10:26:24 +0300

    загрузил проект для тестов

---
+++ FigureSquare.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace FigureSquare.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- FigureSquare/Interfaces/IFigure.cs
+++ FigureSquare/Interfaces/IFigure.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FigureSquare.Interfaces
 {
-    interface IFigure
+    public interface IFigure
     {
         double GetArea();
     }
